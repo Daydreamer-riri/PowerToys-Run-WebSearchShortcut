9d5db5b89274e618e0236e0df60de08d4675bb5a
Tortoise <195262249+Testudinidae@users.noreply.github.com>
2025-07-20 20:24:47 -0700

    feat: add DuckDuckGo to SuggestionsProvider (#51)

--- CmdPalWebSearchShortcut/WebSearchShortcut/Suggestion.cs
+++ CmdPalWebSearchShortcut/WebSearchShortcut/Suggestion.cs
@@ -36,6 +36,7 @@ public class Suggestions
       {
         { Google.Name, new Google() },
         { Bing.Name, new Bing() },
+        { DuckDuckGo.Name, new DuckDuckGo() },
         { YouTube.Name, new YouTube() },
         { Wikipedia.Name, new Wikipedia() },
         { Npm.Name, new Npm() },
